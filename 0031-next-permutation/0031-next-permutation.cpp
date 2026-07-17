9d6ca46a143c640342f01379d90d1bcb53e4b6de
Sharad Chandra Reddy <97039791+SCR01@users.noreply.github.com>
2026-07-17 18:23:33 +0530

    Time: 0 ms (100%), Space: 15.6 MB (73.43%) - LeetHub

--- 0031-next-permutation/0031-next-permutation.cpp
+++ 0031-next-permutation/0031-next-permutation.cpp
@@ -5,12 +5,13 @@ public:
         int ind = -1;
         for(int i = n-2;i>=0;i--){
             if(nums[i]<nums[i+1]){
-                ind = i;
-                break;
+              ind = i;
+            break;
             }
+           
         }
-        if(ind  == -1){
-            reverse(nums.begin(), nums.end());
+        if(ind == -1){
+            reverse(nums.begin(),nums.end());
             return;
         }
         for(int i = n-1;i>ind;i--){
