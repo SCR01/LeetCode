fb7589171345dcdf9686be2501b1baab59decf89
Sharad Chandra Reddy <97039791+SCR01@users.noreply.github.com>
2026-08-04 08:10:53 +0530

    Time: 0 ms (100%), Space: 21 MB (31.16%) - LeetHub

---
+++ 0073-set-matrix-zeroes/0073-set-matrix-zeroes.cpp
@@ -0,0 +1,39 @@
+class Solution {
+public:
+    void setZeroes(vector<vector<int>>& matrix) {
+
+        int n = matrix.size();
+        int m = matrix[0].size();
+
+        int col0 = 1;
+
+        // Step 1: Mark rows and columns
+        for(int i = 0; i < n; i++) {
+
+            if(matrix[i][0] == 0)
+                col0 = 0;
+
+            for(int j = 1; j < m; j++) {
+
+                if(matrix[i][j] == 0) {
+
+                    matrix[i][0] = 0;
+                    matrix[0][j] = 0;
+                }
+            }
+        }
+
+        // Step 2: Fill the matrix using the markers
+        for(int i = n - 1; i >= 0; i--) {
+
+            for(int j = m - 1; j >= 1; j--) {
+
+                if(matrix[i][0] == 0 || matrix[0][j] == 0)
+                    matrix[i][j] = 0;
+            }
+
+            if(col0 == 0)
+                matrix[i][0] = 0;
+        }
+    }
+};
